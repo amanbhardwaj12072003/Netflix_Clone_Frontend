ad25043e3c7ee045a8f1bcaa7780a21ee408d176
amanbhardwaj12072003 <amanbhardwaj12072003@gmail.com>
2023-04-15 18:17:27 +0530

    Navbar Done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,17 @@
-import { Notifications, Search } from "@material-ui/icons";
+import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
 import "./navbar.scss";
+import { useState, useEffect } from "react";
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+
   return (
-    <div className="navbar">
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
       <div className="container">
         <div className="left">
           <img
@@ -17,9 +25,21 @@ const Navbar = () => {
           <span>My List</span>
         </div>
         <div className="right">
-            <Search />
-            <span>KID</span>
-            <Notifications />
+          <Search className="icon" />
+          <span>KID</span>
+          <Notifications className="icon" />
+          <img
+            src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+          />
+
+          <div className="profile">
+            <ArrowDropDown className="icon" />
+            <div className="options">
+              <span>Settings</span>
+              <span>Logout</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
