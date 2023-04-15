a95cafd2a0fb898c6eac9690c67ab90a275af0e4
amanbhardwaj12072003 <amanbhardwaj12072003@gmail.com>
2023-04-16 04:04:28 +0530

    Slider Done :)

---
+++ src/components/listItem/ListItem.jsx
@@ -0,0 +1,41 @@
+import {
+  Add,
+  PlayArrow,
+  ThumbUpAltOutlined,
+  ThumbDownOutlined,
+} from "@material-ui/icons";
+import "./listItem.scss";
+
+const ListItem = () => {
+  return (
+    <div className="listItem">
+      <img
+        src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee"
+        alt=""
+      />
+
+      <div className="itemInfo">
+        <div className="icons">
+          <PlayArrow />
+          <Add />
+          <ThumbUpAltOutlined />
+          <ThumbDownOutlined />
+        </div>
+        <div className="itemInfoTop">
+          <span>1 hour 14 mins</span>
+          <span className="limit">+18</span>
+          <span>2016</span>
+        </div>
+
+        <div className="desc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ea, vero
+          neque minus nemo dolorem consequatur illo eaque.
+        </div>
+
+        <div className="genre">Action</div>
+      </div>
+    </div>
+  );
+};
+
+export default ListItem;
